0e45efeb264acec25550375b870aaa30ad1e82d9
thiago morgado <thiago_morgado@hotmail.com>
2023-12-06 03:27:14 -0300

    feat: adiconado thema escuro e troca de thema

--- src/main.tsx
+++ src/main.tsx
@@ -3,17 +3,16 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App.tsx'
 
 import {BrowserRouter} from 'react-router-dom'
-import { ThemeProvider } from '@mui/material'
-import { LightTheme } from './shared/theme/Light.ts'
+import { AppThemeProvider } from './shared/contexts/themeContext.tsx'
+
 
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <ThemeProvider theme={LightTheme}>
-      <BrowserRouter>
+    <AppThemeProvider>
+     <BrowserRouter>
         <App />
       </BrowserRouter>
-    </ThemeProvider>
-    
+    </AppThemeProvider>
   </React.StrictMode>,
 )
