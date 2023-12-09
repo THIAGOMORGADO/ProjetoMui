8a3083ee9b8206be4729896f521fdf4132fddf6e
thiago morgado <thiago_morgado@hotmail.com>
2023-12-09 02:08:48 -0300

    feat: adicionando resposividade e menu lateral no contexto

--- src/main.tsx
+++ src/main.tsx
@@ -4,13 +4,16 @@ import { App } from './App.tsx'
 
 import {BrowserRouter} from 'react-router-dom'
 import { AppThemeProvider } from './shared/contexts/themeContext.tsx'
+import { DrawerProvider } from './shared/contexts/DrawerContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <AppThemeProvider>
+    <DrawerProvider >
      <BrowserRouter>
         <App />
       </BrowserRouter>
+      </DrawerProvider>
     </AppThemeProvider>
   </React.StrictMode>,
 )
